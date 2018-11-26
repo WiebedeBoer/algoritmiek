291873344ed2bbac58f7c468d46eae4c7e6c3406
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-11-26 19:09:52 +0100

    opg5 setup
    opg5 setup

---
+++ algoritme5/algoritme5/ttt_ab.cpp
@@ -0,0 +1,57 @@
+#include "pch.h"
+#include <iostream>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <limits>
+
+enum class PlayerType { Human, Computer };
+
+int eval(const State &board, const Player &player)
+{
+}
+
+Move alphaBeta(const State &board, int ply)
+{
+}
+
+int main()
+{
+	std::srand(std::time(0));
+
+	std::map<Player, PlayerType> playerType;
+	playerType[Player::X] = PlayerType::Human;
+	playerType[Player::O] = PlayerType::Computer;
+
+	State board = {
+		Player::None, Player::None, Player::None,
+		Player::None, Player::None, Player::None,
+		Player::None, Player::None, Player::None };
+	std::cout << board << std::endl;
+
+	std::vector<Move> moves = getMoves(board);
+	while (moves.size() > 0) {
+		if (playerType[getCurrentPlayer(board)] == PlayerType::Human) {
+			std::cout << "+-+-+-+" << std::endl <<
+				"|0|1|2|" << std::endl <<
+				"+-+-+-+" << std::endl <<
+				"|3|4|5|" << std::endl <<
+				"+-+-+-+" << std::endl <<
+				"|6|7|8|" << std::endl <<
+				"+-+-+-+" << std::endl << std::endl;
+			std::cout << "Enter a move ( ";
+			for (Move m : moves) std::cout << m << " ";
+			std::cout << "): ";
+			Move m;
+			std::cin >> m;
+			board = doMove(board, m);
+		}
+		else {
+			board = doMove(board, alphaBeta(board, 5));
+		}
+		std::cout << board << std::endl;
+		moves = getMoves(board);
+	}
+
+	return 0;
+}
