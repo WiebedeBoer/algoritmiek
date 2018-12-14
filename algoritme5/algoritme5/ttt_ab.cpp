e34d6b59e56926d3661b7a5054acfaea3d7343f8
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-12-14 13:33:34 +0100

    tic tac toe
    tic tac toe

--- algoritme5/algoritme5/ttt_ab.cpp
+++ algoritme5/algoritme5/ttt_ab.cpp
@@ -1,19 +1,90 @@
 #include "pch.h"
 #include <iostream>
-#include <iostream>
 #include <algorithm>
 #include <map>
 #include <limits>
 
 enum class PlayerType { Human, Computer };
 
-int eval(const State &board, const Player &player)
-{
+#define MIN std::numeric_limits<int>::min() + 1 //min
+#define MAX std::numeric_limits<int>::max() //max
+
+using Rating = std::tuple<int, Move>; //rating of move
+unsigned const n_trials = 1000; //number of trials
+
+//get random
+int getRandomEval(const State &board, Player) {
+	return rand() % 101 - 50;
 }
 
+//eval, board player
+int eval(const State &board, const Player &player)
+{
+	auto moves = getMoves(board);
+	int wins = 0;
+
+	for (int i = 0; i < n_trials; ++i) {
+		State mcBoard = board;
+
+		std::random_shuffle(moves.begin(), moves.end()); //do shuffle
+		for (const Move &m : moves) mcBoard = doMove(mcBoard, m);
+
+		if (getWinner(mcBoard) == player) wins++; //count up
+	}
+
+	return wins - n_trials / 2; //number trials divide by 2,  2 players
+}
+
+//rating ab i of move, add min max parameters
+Rating alphaBeta(const State &board, int ply, Player player, int min = MIN, int max = MAX) {
+	auto moves = getMoves(board);
+	Rating bestRating = std::make_tuple(min, Move());
+
+	if (!ply || !moves.size()) return std::make_tuple(eval(board, player), Move());
+
+	for (Move &m : moves) {
+		Rating rating;
+		Player other = player == Player::O ? Player::X : Player::O;
+
+		rating = alphaBeta(doMove(board, m), ply - 1, other, -max, -min);
+
+		if (-std::get<0>(rating) > min) {
+			min = -std::get<0>(rating);
+			bestRating = std::make_tuple(min, m);
+		}
+
+		if (min >= max) return bestRating;
+	}
+
+	return bestRating;
+}
+
+/*
+//move,
 Move alphaBeta(const State &board, int ply)
 {
+	auto moves = getMoves(board);
+	Rating bestRating = std::make_tuple(min, Move());
+
+	if (!ply || !moves.size()) return std::make_tuple(eval(board, player), Move());
+
+	for (Move &m : moves) {
+		Rating rating;
+		Player other = player == Player::O ? Player::X : Player::O;
+
+		rating = alphaBeta(doMove(board, m), ply - 1, other, -max, -min);
+
+		if (-std::get<0>(rating) > min) {
+			min = -std::get<0>(rating);
+			bestRating = std::make_tuple(min, m);
+		}
+
+		if (min >= max) return bestRating;
+	}
+
+	return bestRating;
 }
+*/
 
 int main()
 {
